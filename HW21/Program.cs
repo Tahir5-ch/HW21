4df68d65f5e48296e5f7a631a8b069f42d23bb1c
tahir <user.email>
2024-11-02 18:00:04 +0400

    All Answers

---
+++ HW21/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HW21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region T1
+            ////1.Verilmiş Arrayın elementlərinin cəmini ekrana çap edin
+            //int[] a = { 12, 3, 5, 4, 7, 16, 34 };
+            //int sum = default;
+            //foreach(int i in a)
+            //{
+            //    sum+= i;
+            //}
+            //Console.WriteLine(sum);
+
+
+            #endregion
+            #region T2
+            ////2.Verilmiş int Arrayın ən böyük dəyərə sahib elementini tapın
+            //int[] a = { 12, 6, 54, 79, 32, 158 ,54,87};
+            //Array.Sort(a);
+            //Console.WriteLine(a[a.Length-1]);
+            #endregion
+            #region T3
+            //3.Verilmiş Arrayın ilk və son elementlərinin cəmini tapın
+            //int[] a = { 1, 2, 65, 12, 74, 25 };
+            //Console.WriteLine(a[0] + a[a.Length-1]);
+            #endregion
+            #region T4
+
+            //int a = 10;
+            //int b = 2;
+
+
+            //while (b <= a)
+            //{
+            //    b = b * 2;
+            //    if (b == a)
+            //    {
+            //        Console.WriteLine("quvvetidir");
+            //        break;
+            //    }
+
+
+
+            //}if(b != a)
+            //{
+            //    Console.WriteLine("quvveti deyil");
+            //}
+
+            #endregion
+            #region T5
+            //5.Verilmiş n tam ədədinin neçə mərtəbəli olduğunu tapın. Məs: 23452, output: 5   
+            //int a = 1010;
+            //int count = default;
+            //while (a > 0)
+            //{
+            //    a=a/10;
+            //    count++;
+
+            //}
+            //Console.WriteLine(count);
+            #endregion
+            #region T6
+            // Verilmiş artan sıra ilə düzülmüş ədədlər siyahısındaki n ədədinin yerləşdiryi indexi tapan alqoritm
+            // (meselen 50 axtaririq egər siyahıda 50 ədədi yoxdursa -1 print olsun, varsa var olduğu index)
+            //int[] a = { 12, 36, 52, 14, 8, 97 };
+            //int n = 8;
+            //int p = default;
+            //Array.Sort(a);
+
+            //for(int i = 0; i < a.Length; i++)
+            //{
+               
+                
+            //    if (a[i] == n)
+            //    {
+            //        Console.WriteLine(i);
+
+                    
+            //    }
+            //    else
+            //    {
+            //        p++;
+
+            //    }
+            //    if (p == a.Length)
+            //    {
+            //        Console.WriteLine("-1");
+            //    }
+                
+            //}
+            
+           
+           
+
+
+            #endregion
+        }
+    }
+}
